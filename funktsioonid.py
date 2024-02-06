b3516acf279d5cb998f63da64cd99cf1e9eff308
spookeeyy <152895833+spookeeyy@users.noreply.github.com>
2024-02-06 16:43:32 +0200

    Add files via upload

---
+++ funktsioonid.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+def trükiAB():
+    print("A")
+   # print("B")
+    
+def funktsioon_b():
+    trükiAB()
+    print("B")
+    
+def loealla():
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        sleep(1)
+
+# print("C")
+# trükiAB()
+# print("D")
+# trükiAB()
+
+
+
+
+
+    
+    
